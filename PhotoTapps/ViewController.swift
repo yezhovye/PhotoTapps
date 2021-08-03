a60e2f732d3117b1863baa1a455275bd5d8c8bd3
Sawwaty <yezhovye@mac.com>
2021-08-03 09:29:07 +0300

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Victoria Klimova on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
